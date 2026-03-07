8c4db8b8597e31eaa538aa89de429e76a6e47afe
qwe <22011705@sju.ac.kr>
2026-03-07 11:35:47 +0900

    빌드 오류 해결

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,6 +1,11 @@
-export const SEO = {
+export const SEO: {
+  siteName: string;
+  siteNameKo: string;
+  companyName: string;
+  baseKeywords: string[];
+} = {
   siteName: "finel",
   siteNameKo: "파인엘",
   companyName: "산업용 공압 부품 전문 기업",
   baseKeywords: ["finel", "파인엘", "공압 부품", "산업용 부품"],
-} as const;
+};
