4f1da05ac8f845c11af224b5ed8ff10b72ba3b62
qwe <22011705@sju.ac.kr>
2026-04-04 13:04:38 +0900

    fix : sns 이름 수정 및 사이트 교체. seo메타데이터 수정

--- src/constants/partners.ts
+++ src/constants/partners.ts
@@ -3,7 +3,7 @@
 
 export const PARTNERS = [
     { id: 1, name: "imi", logo: "/partners/imi.png", url: "https://www.norgren.com/en" },
-    { id: 2, name: "sns", logo: "/partners/sns.png", url: "https://snspneumatic.com" },
+    { id: 2, name: "sns pneumatic", logo: "/partners/sns.png", url: "https://www.sns1999.com/" },
     { id: 3, name: "cypag", logo: "/partners/cypag.png", url: "https://cypag.com/en/home-en/" },
     { id: 4, name: "parker", logo: "/partners/parker.png", url: "https://www.parker.com/kr/ko/home.html" },
     { id: 5, name: "kcc", logo: "/partners/kcc.png", url: "http://www.kccpr.com" },
